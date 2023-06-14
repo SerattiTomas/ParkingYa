2f80c07f0f27a5524add06ab74b70c1b77e6ae99
tseratti <tseratti@tdi.com.ar>
2023-06-14 18:09:58 -0300

    imagenes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appId: 'oemspot.parkingya',
+  appName: 'ParkingYa',
   webDir: 'www',
   bundledWebRuntime: false
 };
